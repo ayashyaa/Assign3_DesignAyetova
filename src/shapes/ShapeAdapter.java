0c1c9dbd93e3335ea4988f0393c3e117efca24a6
ayashayaa <153350006+ayashyaa@users.noreply.github.com>
2025-09-28 21:44:24 +0500

    Update ShapeAdapter.java

--- src/shapes/ShapeAdapter.java
+++ src/shapes/ShapeAdapter.java
@@ -1,4 +1,25 @@
 package shapes;
 
-public class ShapeAdapter {
+public class ShapeAdapter implements ShapeDrawer {
+    private OldShapeDrawer oldDrawer;
+
+    public ShapeAdapter() {
+        this.oldDrawer = new OldShapeDrawer();
+    }
+    @Override
+    public void drawCircle() {
+        oldDrawer.drawCircle(); // используем старую реализацию
+    }
+    @Override
+    public void drawSquare() {
+        oldDrawer.drawSquare(); // используем старую реализацию
+    }
+    @Override
+    public void drawTriangle() {
+        System.out.println("Drawing a Triangle (New way)");
+    }
+    @Override
+    public void drawEllipse() {
+        System.out.println("Drawing an Ellipse (New way)");
+    }
 }
