42c5e384cf1559b654023d987f3590c5126c1aaa
ayashayaa <153350006+ayashyaa@users.noreply.github.com>
2025-09-28 21:44:04 +0500

    Update OldShapeDrawer.java

--- src/shapes/OldShapeDrawer.java
+++ src/shapes/OldShapeDrawer.java
@@ -1,4 +1,13 @@
 package shapes;
 
 public class OldShapeDrawer {
+    public void drawCircle() {
+        System.out.println("Drawing a Circle (Old way)");
+    }
+
+    public void drawSquare() {
+        System.out.println("Drawing a Square (Old way)");
+    }
 }
+
+
