92a4864479b3eeac47d1b0f75cc3c1534cba5ebd
ayashayaa <153350006+ayashyaa@users.noreply.github.com>
2025-09-28 21:43:46 +0500

    Update Main.java

--- src/shapes/Main.java
+++ src/shapes/Main.java
@@ -1,4 +1,12 @@
 package shapes;
 
 public class Main {
+    public static void main(String[] args) {
+        ShapeDrawer drawer = new ShapeAdapter();
+
+        drawer.drawCircle();   // Drawing a Circle (Old way)
+        drawer.drawSquare();   // Drawing a Square (Old way)
+        drawer.drawTriangle(); // Drawing a Triangle (New way)
+        drawer.drawEllipse();  // Drawing an Ellipse (New way)
+    }
 }
